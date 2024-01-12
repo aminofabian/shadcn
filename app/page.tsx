f1c3f4140bec20e9d6a41870cfde409574a823e5
fabianamino <aminofabian@gmail.com>
2024-01-12 19:58:13 +0300

    shortened

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,12 @@
+import MyDialog from '@/components/myComponents/MyDialog'
 import Image from 'next/image'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
-import { buttonVariants } from "@/components/ui/button"
-
 
 
 
 export default function Home() {
   return (
     <>
-    
-    <Dialog>
-    <DialogTrigger>
-    <Button>
-    Click Here
-    </Button>    
-    </DialogTrigger>
-    <DialogContent>
-    <DialogHeader>
-    <DialogTitle>Are you absolutely sure?</DialogTitle>
-    <DialogDescription>
-    This action cannot be undone. This will permanently delete your account
-    and remove your data from our servers.
-    </DialogDescription>
-    </DialogHeader>
-    </DialogContent>
-    </Dialog>
-    
+    <MyDialog />
     
     </>
     
--- components/myComponents/MyDialog.tsx
+++ components/myComponents/MyDialog.tsx
@@ -1,9 +1,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 
@@ -14,13 +11,7 @@ function MyDialog() {
     <DialogTrigger>
     </DialogTrigger>
     <DialogContent>
-    <DialogHeader>
-    <DialogTitle>Are you absolutely sure?</DialogTitle>
-    <DialogDescription>
-    This action cannot be undone. This will permanently delete your account
-    and remove your data from our servers.
-    </DialogDescription>
-    </DialogHeader>
+    Only My Content
     </DialogContent>
     </Dialog>
     
