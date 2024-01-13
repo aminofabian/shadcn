e6980d815c8cc8224f716e802eb94c3a26252de0
fabianamino <aminofabian@gmail.com>
2024-01-13 18:03:54 +0300

    created a hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import MyDialog from '@/components/myComponents/MyDialog'
-import Image from 'next/image'
+import React from 'react';
 
-
-
-export default function Home() {
+const Hero = () => {
   return (
-    <>
-    <MyDialog />
-    
-    </>
-    
-    )
-  }
+    <div className="bg-slate-500 text-white h-screen flex items-center justify-center">
+    <div className="text-center">
+    <h1 className="text-4xl font-bold mb-4">Your Hero Title</h1>
+    <p className="text-lg mb-8">A brief description of your awesome project.</p>
+    <button className="bg-yellow-500 text-blue-500 py-2 px-4 rounded-full hover:bg-yellow-400 transition duration-300">
+    Get Started
+    </button>
+    </div>
+    </div>
+    );
+  };
+  
+  export default Hero;
   
