3fabb456d7c15375c0eadfa3dea6a5194363711a
fabianamino <aminofabian@gmail.com>
2024-01-13 19:36:44 +0300

    added a toggle button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ShadCNThemeProvider } from '@/providers/shadCNProvider/shadCNProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <ShadCNThemeProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem
+    disableTransitionOnChange
+    >
+    {children}
+    </ShadCNThemeProvider>
+    </body>
     </html>
-  )
-}
+    )
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
+import Hero from '@/components/Hero'
+import React from 'react'
 
-const Hero = () => {
+function page() {
   return (
-    <div className="bg-slate-500 text-white h-screen flex items-center justify-center">
-    <div className="text-center">
-    <h1 className="text-4xl font-bold mb-4">Your Hero Title</h1>
-    <p className="text-lg mb-8">A brief description of your awesome project.</p>
-    <button className="bg-yellow-500 text-blue-500 py-2 px-4 rounded-full hover:bg-yellow-400 transition duration-300">
-    Get Started
-    </button>
+    <div>
+    <Hero />
+    
     </div>
-    </div>
-    );
-  };
+    )
+  }
   
-  export default Hero;
-  
+  export default page
